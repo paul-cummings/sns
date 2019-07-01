b3bcba2efadca20ceda65627ceb3deab82da2ea8
Paul Cummings <paul.cummings@bazaarvoice.com>
2019-07-01 09:21:47 +0100

    added filter feature

--- features/filter.feature
+++ features/filter.feature
@@ -12,5 +12,5 @@ Feature: Filter
       | priority   | String    | high         |
     When I get subscription attributes for "subscription"
     And I see attribute "FilterPolicy" with value "JSON"
-    And I see message attribute "FilterPolicy" with value "JSON"
+    Then I should see "c1" in queue "queue"
     Then The publish request should be successful
--- features/step_definitions/filter.rb
+++ features/step_definitions/filter.rb
@@ -1,8 +1,10 @@
 And(/^I see message attribute "([^"]*)" with value "([^"]*)"$/) do |name, an, av|
+  response = $SQS.receive_message
+
   if $SNS.get_subscription_attributes({
       subscription_arn: @namedSubscriptionArns[name],
       attribute_name: an,
       attribute_value: av
-                                       }).include?
+                                       })
   end
 end
